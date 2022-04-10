b1447beed2ee01823da4568034d082db39349268
andisuartika <pt.andisuartika@gmail.com>
2022-04-10 12:01:36 +0800

    Nasabah Screen

--- lib/screens/tarik/tarik_konfirmasi_screen.dart
+++ lib/screens/tarik/tarik_konfirmasi_screen.dart
@@ -1,9 +1,14 @@
+import 'package:ewaste/widgets/custom_button.dart';
+import 'package:ewaste/widgets/loading_button.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:intl/intl.dart';
 
 import '../../theme.dart';
 
 class TarikKonfirmasiScreen extends StatefulWidget {
   final int idBank;
+  final String bankName;
   final String norek;
   final String name;
   final int nominal;
@@ -13,6 +18,7 @@ class TarikKonfirmasiScreen extends StatefulWidget {
     required this.norek,
     required this.name,
     required this.nominal,
+    required this.bankName,
   }) : super(key: key);
 
   @override
@@ -20,8 +26,352 @@ class TarikKonfirmasiScreen extends StatefulWidget {
 }
 
 class _TarikKonfirmasiScreenState extends State<TarikKonfirmasiScreen> {
+  final formkey = GlobalKey<FormState>();
+  TextEditingController passController = TextEditingController(text: '');
+
+  bool isLoading = false;
+  bool isVisible = true;
+
+  // HIDDEN PASSWORD
+  passwordHidden() async {
+    setState(() {
+      isVisible = !isVisible;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    // SUCCESS DIALOG
+    Future<void> showSuccessDialog() async {
+      return showDialog(
+        context: context,
+        builder: (BuildContext context) => Container(
+          width: MediaQuery.of(context).size.width - (2 * defaultMargin),
+          child: AlertDialog(
+            backgroundColor: whiteColor,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(30),
+            ),
+            content: SingleChildScrollView(
+              child: Column(
+                children: [
+                  Align(
+                    alignment: Alignment.centerLeft,
+                    child: GestureDetector(
+                      onTap: () {
+                        Navigator.pop(context);
+                      },
+                      child: Icon(
+                        Icons.close,
+                        color: primaryTextColor,
+                      ),
+                    ),
+                  ),
+                  Image.asset(
+                    'assets/icon_success.png',
+                    width: 100,
+                  ),
+                  SizedBox(
+                    height: 12,
+                  ),
+                  Text(
+                    'Terima Kasih',
+                    style: primaryTextStyle.copyWith(
+                      fontSize: 18,
+                      fontWeight: semiBold,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 12,
+                  ),
+                  Text(
+                    'Transaksi Berhasil disimpan',
+                    style: secondaryTextStyle,
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Container(
+                    width: 154,
+                    height: 44,
+                    child: TextButton(
+                      onPressed: () {
+                        Navigator.pushNamed(context, '/main');
+                      },
+                      style: TextButton.styleFrom(
+                        backgroundColor: primaryColor,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                      ),
+                      child: Text(
+                        'Beranda',
+                        style: whiteTextStyle.copyWith(
+                          fontSize: 16,
+                          fontWeight: medium,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      );
+    }
+
+    handleTransaksi() async {
+      setState(() {
+        isLoading = true;
+      });
+
+      showSuccessDialog();
+
+      setState(() {
+        isLoading = false;
+      });
+    }
+
+    // RINCIAN PENARIKAN
+    Widget detailPenarikan() {
+      return Container(
+        margin: EdgeInsets.only(top: 30),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Detail Penarikan Poin',
+              style: primaryTextStyle.copyWith(
+                fontSize: 14,
+                fontWeight: semiBold,
+              ),
+            ),
+            SizedBox(
+              height: 15,
+            ),
+            Container(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Penerima',
+                    style: primaryTextStyle.copyWith(
+                      fontSize: 12,
+                      fontWeight: semiBold,
+                    ),
+                  ),
+                  Row(
+                    children: [
+                      Expanded(
+                        child: Text(
+                          'Nama',
+                          style: secondaryTextStyle.copyWith(
+                            fontSize: 12,
+                            fontWeight: regular,
+                          ),
+                        ),
+                      ),
+                      Text(
+                        widget.name.toUpperCase(),
+                        style: secondaryTextStyle.copyWith(
+                          fontSize: 12,
+                          fontWeight: medium,
+                        ),
+                      ),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Expanded(
+                        child: Text(
+                          'Bank',
+                          style: secondaryTextStyle.copyWith(
+                            fontSize: 12,
+                            fontWeight: regular,
+                          ),
+                        ),
+                      ),
+                      Text(
+                        '${widget.bankName} | ${widget.norek}',
+                        style: secondaryTextStyle.copyWith(
+                          fontSize: 12,
+                          fontWeight: medium,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Container(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Jumlah',
+                    style: primaryTextStyle.copyWith(
+                      fontSize: 12,
+                      fontWeight: semiBold,
+                    ),
+                  ),
+                  Row(
+                    children: [
+                      Expanded(
+                        child: Text(
+                          'Penarikan poin',
+                          style: secondaryTextStyle.copyWith(
+                            fontSize: 12,
+                            fontWeight: regular,
+                          ),
+                        ),
+                      ),
+                      Text(
+                        NumberFormat.currency(
+                          locale: 'id',
+                          symbol: 'Rp',
+                          decimalDigits: 0,
+                        ).format(widget.nominal),
+                        style: primaryTextStyle.copyWith(
+                          fontSize: 12,
+                          fontWeight: medium,
+                        ),
+                      ),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Expanded(
+                        child: Text(
+                          'Biaya administrasi',
+                          style: secondaryTextStyle.copyWith(
+                            fontSize: 12,
+                            fontWeight: regular,
+                          ),
+                        ),
+                      ),
+                      Text(
+                        NumberFormat.currency(
+                          locale: 'id',
+                          symbol: 'Rp',
+                          decimalDigits: 0,
+                        ).format(5000),
+                        style: secondaryTextStyle.copyWith(
+                          fontSize: 12,
+                          fontWeight: medium,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            Divider(
+              thickness: 1,
+              color: secondaryTextColor,
+            ),
+            Row(
+              children: [
+                Expanded(
+                  child: Text(
+                    'Total',
+                    style: secondaryTextStyle.copyWith(
+                      fontSize: 12,
+                      fontWeight: regular,
+                    ),
+                  ),
+                ),
+                Text(
+                  NumberFormat.currency(
+                    locale: 'id',
+                    symbol: 'Rp',
+                    decimalDigits: 0,
+                  ).format(widget.nominal + 5000),
+                  style: primaryTextStyle.copyWith(
+                    fontSize: 14,
+                    fontWeight: medium,
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
+      );
+    }
+
+    // KATA SANDI
+    Widget kataSandi() {
+      return Container(
+        margin: EdgeInsets.only(top: 30),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Masukkan kata sandi untuk konfirmasi',
+              style: primaryTextStyle.copyWith(
+                fontSize: 14,
+                fontWeight: semiBold,
+              ),
+            ),
+            SizedBox(
+              height: 3,
+            ),
+            Container(
+              width: double.infinity,
+              child: TextFormField(
+                cursorColor: primaryTextColor,
+                controller: passController,
+                obscureText: isVisible,
+                autovalidateMode: AutovalidateMode.onUserInteraction,
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return "Masukkan kata sandi";
+                  }
+                  return null;
+                },
+                decoration: InputDecoration(
+                  contentPadding: EdgeInsets.fromLTRB(0.0, 10.0, 20.0, 10.0),
+                  hintText: 'Masukkan Kata sandi',
+                  hintStyle: hintTextStyle.copyWith(
+                    fontSize: 12,
+                    fontWeight: regular,
+                  ),
+                  fillColor: backgorundFieldColor,
+                  filled: true,
+                  border: OutlineInputBorder(
+                    borderSide: BorderSide.none,
+                    borderRadius: BorderRadius.circular(
+                      12,
+                    ),
+                  ),
+                  prefixIcon: SvgPicture.asset(
+                    'assets/icon_password.svg',
+                    width: 10,
+                    height: 10,
+                    fit: BoxFit.scaleDown,
+                  ),
+                  suffixIcon: GestureDetector(
+                    onTap: passwordHidden,
+                    child: SvgPicture.asset(
+                      isVisible
+                          ? 'assets/icon_password_eye_hidden.svg'
+                          : 'assets/icon_password_eye.svg',
+                      width: 18,
+                      height: 18,
+                      fit: BoxFit.scaleDown,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      );
+    }
+
     return Scaffold(
       backgroundColor: whiteColor,
       appBar: AppBar(
@@ -43,6 +393,32 @@ class _TarikKonfirmasiScreenState extends State<TarikKonfirmasiScreen> {
       ),
       body: Container(
         margin: EdgeInsets.symmetric(horizontal: 30),
+        child: Form(
+          key: formkey,
+          child: Column(
+            children: [
+              detailPenarikan(),
+              kataSandi(),
+            ],
+          ),
+        ),
+      ),
+      bottomNavigationBar: Container(
+        margin: EdgeInsets.symmetric(
+          horizontal: 30,
+          vertical: 30,
+        ),
+        child: isLoading
+            ? LoadingButton()
+            : CustomButton(
+                text: 'Lanjutkan',
+                color: primaryColor,
+                press: () {
+                  if (formkey.currentState!.validate()) {
+                    handleTransaksi();
+                  }
+                },
+              ),
       ),
     );
   }
